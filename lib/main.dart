dddb32efc318863caa34a89c7d33cade632405f8
yeabsera_seyoum <yeabsera.seyoum@bitscollege.edu.et>
2024-01-02 11:20:19 +0300

    Sign up UI

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
           useMaterial3: true,
         ),
-        home: Splash());
+        home: const Splash());
   }
 }
 
