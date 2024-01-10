9b43d27e04f0adb8059d545e77e324a0ffb4c221
yeabsera_seyoum <yeabsera.seyoum@bitscollege.edu.et>
2024-01-10 10:13:57 +0300

    something changed

---
+++ lib/models/products.dart
@@ -0,0 +1,32 @@
+List<Map<String, Object>> product = [
+  {
+    "id": 1,
+    "name": "Product 1",
+    "price": 100,
+    "image": "https://picsum.photos/200/300",
+  },
+  {
+    "id": 2,
+    "name": "Product 2",
+    "price": 100,
+    "image": "https://picsum.photos/200/300",
+  },
+  {
+    "id": 3,
+    "name": "Product 3",
+    "price": 100,
+    "image": "https://picsum.photos/200/300",
+  },
+  {
+    "id": 4,
+    "name": "Product 4",
+    "price": 100,
+    "image": "https://picsum.photos/200/300",
+  },
+  {
+    "id": 5,
+    "name": "Product 5",
+    "price": 100,
+    "image": "https://picsum.photos/200/300",
+  }
+];
